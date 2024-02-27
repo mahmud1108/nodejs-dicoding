a10ce3fdd33738fd1b13ae70cadc898e1acb5289
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-02-27 23:15:03 +0700

    host

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { routes } from "./routes.js";
 const init = async () => {
 	const server = Hapi.server({
 		port: 5000,
-		host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+		host: process.env.NODE_ENV !== "production" ? "0.0.0.0" : "localhost",
 		routes: {
 			cors: {
 				origin: ["*"],
